42f59af28da7a633b9889f0244d0dc5f796564e5
Skanda <skandakk@gmail.com>
2016-07-21 16:48:24 -0400

    animation patch. lines erase successfully. check code to see what was changed

--- drawing.js
+++ drawing.js
@@ -36,7 +36,7 @@ function plsWork()
     function animate() {
     	if (variable>=10000000) clearInterval(id);
     	else {
-			
+			context.clearRect(0,0,context.canvas.width, context.canvas.height);
 			d2Theta1  =  (g*(Math.sin(Theta2)*Math.cos(Theta1-Theta2)-mu*Math.sin(Theta1))-(l2*dTheta2*dTheta2+l1*dTheta1*dTheta1*Math.cos(Theta1-Theta2))*Math.sin(Theta1-Theta2))/(l1*(mu-Math.cos(Theta1-Theta2)*Math.cos(Theta1-Theta2)));
 			d2Theta2  =  (mu*g*(Math.sin(Theta1)*Math.cos(Theta1-Theta2)-Math.sin(Theta2))+(mu*l1*dTheta1*dTheta1+l2*dTheta2*dTheta2*Math.cos(Theta1-Theta2))*Math.sin(Theta1-Theta2))/(l2*(mu-Math.cos(Theta1-Theta2)*Math.cos(Theta1-Theta2)));
 			/*alert(mu);
@@ -51,12 +51,11 @@ function plsWork()
 			dTheta2   += d2Theta2*time;
 			Theta1    += dTheta1*time;
 			Theta2    += dTheta2*time;
-			line1={x0: 200, y0: 200, x: line1.x0+(l1*Math.sin(Theta1)), y: line1.y0+(l1*Math.cos(Theta1))};
+			line1={x0: 175, y0: 50, x: line1.x0+(l1*Math.sin(Theta1)), y: line1.y0+(l1*Math.cos(Theta1))};
 			line2={x0: line1.x, y0: line1.y, x: line1.x+l2*Math.sin(Theta2), y: line1.y+l2*Math.cos(Theta2)};
 			drawLine(context,line1.x0, line1.y0, line1.x, line1.y);
 			//alert(d2Theta1);
 			drawLine(context,line2.x0, line2.y0, line2.x, line2.y);
-			clearRect(0,0,context.width, context.height);
 			variable++;
 			
 		
@@ -67,11 +66,11 @@ function plsWork()
 	}
 
 	function drawLine(ctx, startx, starty, endx, endy) {
-		ctx.clearRect(0,0,ctx.width, ctx.height);
+		//ctx.clearRect(0,0,ctx.canvas.width, ctx.canvas.height);
 		ctx.beginPath();
 		ctx.moveTo(startx,starty);
 		ctx.lineTo(endx, endy);
 		ctx.stroke();
 		ctx.closePath();
-		ctx.clearRect(0,0,ctx.width,ctx.height);
+		//ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
 	}
